6a7172b10aafbc401541fe21432a9eaa7a06e2c5
vishal-patidar-02 <vishalpatidar07070707@gmail.com>
2025-02-21 18:31:54 +0530

    Pricing Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Navbar from './components/navbar'
-import HeroSection from './components/heroSection'
-import FeatureSection from './components/FeatureSection'
-import Workflow from './components/Workflow'
+import React from "react";
+import Navbar from "./components/navbar";
+import HeroSection from "./components/heroSection";
+import FeatureSection from "./components/FeatureSection";
+import Pricing from "./components/pricing";
+import Workflow from "./components/Workflow";
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <div className="max-w-7xl mx-auto pt-20 px-6">
-      <HeroSection/>
-      <FeatureSection/>
-      <Workflow/>
-    </div>
-
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
